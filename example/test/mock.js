23b1f213e59c796f8ea6c550b0a001eb410bb832
nelsonic <contact.nelsonic@gmail.com>
2015-02-01 07:21:39 +0000

    outline for our mock methods

---
+++ example/test/mock.js
@@ -0,0 +1,18 @@
+// mock methods for http request & response
+
+// request should have
+
+// req.headers
+
+// req.method
+
+// req.on('data' ... event emitter
+
+// req.on('end'  ... event emitter
+
+
+// response shoud have
+
+// res.writeHead(
+
+// res.end(
