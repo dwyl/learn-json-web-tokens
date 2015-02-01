2227f2c1467891f841ca783d0ee15f6eee4fb12a
nelsonic <contact.nelsonic@gmail.com>
2015-02-01 06:48:24 +0000

    tidy example/server.js and add comments

--- example/server.js
+++ example/server.js
@@ -1,15 +1,15 @@
-var host = "127.0.0.1";
-var port = process.env.PORT || 1337; // let heroku define port
-var http = require('http');
-var e    = require('./helpers'); // auth, token verification & render helpers
+var port = process.env.PORT || 1337; // let heroku define port or use 1337
+var http = require('http');      // core node.js http (no frameworks)
+var app  = require('./helpers'); // auth, token verification & render helpers
 
-http.createServer(function (req, res) {
-  if(req.url === '/')             { e.home(res);          }
-  else if(req.url === '/private') { e.validate(req, res); }
-  else if(req.url === '/auth')    { e.handler(req,res);   }
-  else if(req.url === '/logout')  { e.logout(res);        }
-  else if(req.url === '/exit')    { e.exit(res);          }
-  else                            { e.notFound(res);      }
-}).listen(port, host);
+module.exports = http.createServer(function (req, res) {
+  var url = req.url;
+  if( url === '/' || url === '/home' ) { app.home(res);  } // homepage
+  else if( url === '/private') { app.validate(req, res); } // private content
+  else if( url === '/auth')    { app.handler(req,res);   } // authenticator
+  else if( url === '/logout')  { app.logout(req, res);   } // end session
+  else if( url === '/exit')    { app.exit(res);          } // for testing ONLY
+  else                         { app.notFound(res);      } // 404 error
+}).listen(port);
 
-console.log("Visit: http://"+ host +":" + port);
+console.log("Visit: http://127.0.0.1:" + port);
